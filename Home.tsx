61e7536685d1f5a9a66f2872ff3e6d37ec23f4b8
AmiChanDev <gamageamantha@gmail.com>
2025-08-26 12:47:50 +0530

    update App component to display 'Test' and set Home as the root component

---
+++ Home.tsx
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from "react-native";
+import App from "./App";
+
+export default function Home() {
+    return (
+        <View
+            style={style.view1}
+        >
+
+        </View>
+    )
+}
+
+const style = StyleSheet.create({
+    view1: {
+        flex: 1,
+        backgroundColor: "red"
+    }
+});
