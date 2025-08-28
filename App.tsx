aacc0c9ed763436cb5234e73b6d26b08976b84f4
AmiChanDev <gamageamantha@gmail.com>
2025-08-28 11:25:08 +0530

    Add AsyncStorage functionality for saving and viewing names

--- App.tsx
+++ App.tsx
@@ -1,10 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, Button, FlatList, StyleSheet, Text, View } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Async Storage</Text>
+
+      <Text style={styles.title}>List View</Text>
+      <FlatList
+        data={[{ name: 'Sahan' }, { name: 'Danusi' }, { name: 'Amantha' }]}
+        renderItem={({ item }) => (
+          <Button title={item.name} />
+        )}
+      />
+
+      <Button title='Save' onPress={async () => {
+        try {
+          await AsyncStorage.setItem('name', 'sahan');
+          Alert.alert('done');
+        } catch {
+
+        }
+      }} />
+
+      <Button title='View' onPress={async () => {
+        try {
+          const name = await AsyncStorage.getItem('name');
+          Alert.alert(name ? name : 'No name found');
+        } catch {
+
+        }
+      }} />
+
       <StatusBar style="auto" />
     </View>
   );
